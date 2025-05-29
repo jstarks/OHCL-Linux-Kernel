630f6030a19d6d15f55034352eef27cc916a5a77
John Starks <jostarks@microsoft.com>
2025-05-29 19:15:56 +0000

    drivers/hv/mshv_vtl: detect dropped sidecar remove signal
    In automated testing, it looks like perhaps the sidecar driver is
    failing to notice that the sidecar kernel has removed a VP. This
    causes a hang when onlining the CPU.
    
    To make investigation easier, add some additional logging, plus poll
    periodically in case the signal was dropped. This is not a final fix,
    but it should make this condition easier to debug.
    
    Signed-off-by: John Starks <jostarks@microsoft.com>

--- drivers/hv/mshv_vtl_sidecar.c
+++ drivers/hv/mshv_vtl_sidecar.c
@@ -147,13 +147,18 @@ static int sidecar_remove(unsigned int cpu)
 		return 0;
 	}
 
-	dev_info(dev->dev, "removing sidecar cpu %d", cpu);
+	dev_info(dev->dev, "cpu %d: removing from sidecar", cpu);
 	slot = &dev->control->cpu_status[cpu_index];
 	last = cmpxchg(slot, CPU_STATUS_IDLE, CPU_STATUS_REMOVE);
 	WARN(last != CPU_STATUS_IDLE, "unexpected cpu status %d", last);
 
 	sidecar_signal(dev, cpu);
-	wait_event(dev->wait, READ_ONCE(*slot) == CPU_STATUS_REMOVE);
+	while (!(ret = wait_event_timeout(dev->wait, READ_ONCE(*slot) == CPU_STATUS_REMOVE, HZ / 10)))
+		;
+	if (ret == 1)
+		dev_warn(dev->dev, "cpu %d: missed remove complete signal", cpu);
+	} else
+		dev_info(dev->dev, "cpu %d: remove complete", cpu);
 	last = READ_ONCE(dev->vp_state[cpu_index]);
 	WARN(last != VP_STATE_REMOVED, "unexpected vp state %d", last);
 	return 0;
